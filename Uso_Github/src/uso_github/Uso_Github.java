6935979a5aced6ec477839aef1b9a7a8965e3475
ArielPerezGalvez <arielgalvez70@yahoo.es>
2018-05-02 20:56:43 -0600

    Hacer una salida para un numero

--- Uso_Github/src/uso_github/Uso_Github.java
+++ Uso_Github/src/uso_github/Uso_Github.java
@@ -16,6 +16,8 @@ public class Uso_Github {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int n=5;
+        System.out.println("Prueba:"+n);
     }
     
 }
