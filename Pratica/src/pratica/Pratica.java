fb17d3bdb96a696eaac66eefb32082504e8876c7
ArielPerezGalvez <arielgalvez70@yahoo.es>
2018-05-10 14:36:08 -0600

    Configurando los problemas del programa

---
+++ Pratica/src/pratica/Pratica.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratica;
+import java.util.Scanner;
+/**
+ *
+ * @author ariel
+ */
+public class Pratica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }//fin del main
+    
+}//fin de la clase
